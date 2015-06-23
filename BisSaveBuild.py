97884c4452ebfba925495ab940e3e8487b145281
Ed Winn <ed.winn@northstarmutual.com>
2015-06-22 19:53:41 -0500

    Adding special files

--- BisSaveBuild.py
+++ BisSaveBuild.py
@@ -5,6 +5,7 @@ import os
 
 
 class BisSaveBuild(sublime_plugin.EventListener):
+
     def on_post_save(self, view):
         global_settings = sublime.load_settings('BIS.sublime-settings')
         print
@@ -12,10 +13,12 @@ class BisSaveBuild(sublime_plugin.EventListener):
         # See if we should build. A project level build_on_save setting
         # takes precedence. To be backward compatible, we assume the global
         # build_on_save to be true if not defined.
-        should_build = view.settings().get('build_on_save', global_settings.get('build_on_save', True))
+        should_build = view.settings().get(
+            'build_on_save', global_settings.get('build_on_save', True))
 
         # Load filename filter. Again, a project level setting takes precedence.
-        filename_filter = view.settings().get('filename_filter', global_settings.get('filename_filter', '.*'))
+        filename_filter = view.settings().get(
+            'filename_filter', global_settings.get('filename_filter', '.*'))
         print("Filename filter" + filename_filter)
 
         if not should_build:
@@ -27,13 +30,17 @@ class BisSaveBuild(sublime_plugin.EventListener):
         # Get Path and User variables
         appdata = os.environ['USERPROFILE']
         app = 'sublwatcher'
-        bis_save_file = 'changes.txt'
-        file_path = appdata + '\\' + app + '\\' + bis_save_file
         file_name = view.file_name()
-        # print(file_name)
-        # pos = file_name.find('site-')
-        # pos = pos + 5
-        # site = file_name[pos]
+        print(file_name)
+        pos = file_name.find('site-')
+        pos = pos + 5
+        site = file_name[pos].upper()
+        # print(site)
+        if site == 'A' or site == 'B' or site == 'C' or site == 'D':
+            bis_save_file = 'changes.txt'
+        else:
+            bis_save_file = 'site-' + site + '\\changes.txt'
+        file_path = appdata + '\\' + app + '\\' + bis_save_file
 
         # Write file to path
         with open(file_path, "w") as textfile:
